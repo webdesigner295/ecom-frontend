387072caa735fd22109d3aa71888752a8fcb4ffd
webdesigner295 <webdesigner295@gmail.com>
2023-12-06 23:45:25 +0500

    added api

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,8 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
+const API_URL = "https://ecom-app-api.vercel.app"
 export const BASE_URL = ''; // If using proxy
-export const PRODUCTS_URL = '/api/products';
-export const USERS_URL = '/api/users';
-export const ORDERS_URL = '/api/orders';
-export const PAYPAL_URL = '/api/config/paypal';
+export const PRODUCTS_URL = `${API_URL}/api/products`;
+export const USERS_URL = `${API_URL}/api/users`;
+export const ORDERS_URL = `${API_URL}/api/orders`;
+export const PAYPAL_URL = `${API_URL}/api/config/paypal`;
